02cca9bf0c4bcade32fd666530da45fcb2dedfe7
WYC <wycstar@live.com>
2018-01-22 18:43:35 +0800

    20181221843

--- httpd/main.cpp
+++ httpd/main.cpp
@@ -10,10 +10,6 @@ int main()
     //server.dispatch();
     std::shared_ptr<WSocket> socket = std::make_shared<WSocket>(SOCKET_TYPE::INET);
     socket->connect("127.0.0.1", 9000);
-    /*FCGI::begin_request(socket->native(), 1);
-    FCGI::set_env(socket->native(), 1, "SCRIPT_FILENAME", "/home/wyc/code/html/index.php");
-    FCGI::set_env(socket->native(), 1, "REQUEST_METHOD", "GET");
-    FCGI::end_request(socket->native(), 1);*/
     std::map<std::string, std::string> params = {
         {"SCRIPT_FILENAME", "/home/wyc/code/html/index.php"},
         {"REQUEST_METHOD", "GET"}
